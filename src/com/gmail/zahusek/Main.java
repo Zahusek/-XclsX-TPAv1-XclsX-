ec6141a2a30e8ee23fd8149f85e88c863493268a
Zahusek <zahusek@gmail.com>
2015-04-22 14:18:07 +0200

    Update Main.java

--- src/com/gmail/zahusek/Main.java
+++ src/com/gmail/zahusek/Main.java
@@ -7,6 +7,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import com.gmail.zahusek.api.ServerStatusAPI;
+import com.gmail.zahusek.core.TinyProtocol;
 
 public class Main extends JavaPlugin {
 
