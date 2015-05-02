1b7e2b2e6f6fb5ee1d9a20f5cc5f6a27f29f2fd8
Zahusek <zahusek@gmail.com>
2015-05-02 11:03:48 +0200

    PacketPlayOutPlayerInfo.java - v1.1

---
+++ src/com/gmail/zahusek/packets/PacketPlayOutPlayerInfo.java
@@ -0,0 +1,38 @@
+package com.gmail.zahusek.packets;
+
+import com.gmail.zahusek.protocol.Reflection;
+import com.gmail.zahusek.protocol.TinyProtocol;
+
+public class PacketPlayOutPlayerInfo extends AbstractPacket {
+	
+	private static final Object handle = Reflection.getConstructor("{nms}.PacketPlayOutPlayerInfo").invoke();
+	
+	public PacketPlayOutPlayerInfo() {
+		super(handle, TinyProtocol.getTinyProtocol());
+	}
+    
+	public String getPlayerName() { 
+    	return (String) Reflection.getMethod(packet.getClass(), "a").invoke(packet);
+    }
+    public void setPlayerName(String value) { 
+    	Reflection.getField(packet.getClass(), "a", String.class).set(packet, value); 
+    }
+    
+    public boolean getOnline() { 
+    	return (boolean) Reflection.getMethod(packet.getClass(), "b").invoke(packet);
+    }
+    public void setOnline(boolean value) { 
+    	Reflection.getField(packet.getClass(), "b", boolean.class).set(packet, value);
+    }
+    
+    public int getPing() { 
+    	return (int) Reflection.getMethod(packet.getClass(), "c").invoke(packet);
+    }
+    public void setPing(int value) {
+    	Reflection.getField(packet.getClass(), "c", int.class).set(packet, value);
+    }
+    
+    public void setPacket(String value1, boolean value2, int value3) {
+    	setPlayerName(value1); setOnline(value2); setPing(value3);
+    }
+}
